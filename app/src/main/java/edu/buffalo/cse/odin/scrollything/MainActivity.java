577475ccc111017d25c9bbfcab96270ca9499bc5
gourabmi <gourabmi@buffalo.edu>
2018-08-02 19:22:43 -0400

    Basic RecyclerView working

--- app/src/main/java/edu/buffalo/cse/odin/scrollything/MainActivity.java
+++ app/src/main/java/edu/buffalo/cse/odin/scrollything/MainActivity.java
@@ -2,12 +2,47 @@ package edu.buffalo.cse.odin.scrollything;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+//        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/edu/buffalo/cse/odin/scrollything/MyRecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package edu.buffalo.cse.odin.scrollything;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+    /*
+    (DONE) Refer Basic RecyclerView operations https://stackoverflow.com/questions/26517855/using-the-recyclerview-with-a-database
+    TODO Refer for CursorAdapter and DB Operations https://stackoverflow.com/questions/26517855/using-the-recyclerview-with-a-database
+    TODO Refer for RecyclerView Lifecycle https://stackoverflow.com/questions/37674692/what-is-lifecycle-for-recyclerview-adapter
+     */
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    MyRecyclerViewAdapter(Context context, List<String> data){
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycler_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tvAnimalName);
+//            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+}
